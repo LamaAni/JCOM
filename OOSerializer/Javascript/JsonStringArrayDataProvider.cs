1841d6194d0c924449398e89b5524c631e8733c2
Zav <Zav.Shotan@GMail.com>
2014-12-25 09:29:42 -0500

    First commit of JSON serializer. Partial serialization is now working.

---
+++ OOSerializer/Javascript/JsonStringArrayDataProvider.cs
@@ -0,0 +1,178 @@
+﻿using JCOM.OOSerializer.Reference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JCOM.Core.Strings;
+using JCOM.OOSerializer;
+using JCOM.Runtime;
+
+namespace JCOM.OOSerializer.Javascript
+{
+    public class JsonStringArrayDataProvider : JsonMemoryArrayDataProvider<string>
+    {
+        public JsonStringArrayDataProvider(string source=null, IJsonSerializer<string> serializer = null)
+            : base(serializer == null ? Javascript.JsonStringSerializer.Global : serializer)
+        {
+            ReadSource(source);
+        }
+
+        /// <summary>
+        /// The object delimiter associated with the string source.
+        /// </summary>
+        public static string ObjectDelimiter = "☺";
+
+        /// <summary>
+        /// The spacer for pretty json.
+        /// </summary>
+        public static string PrettySpacer = "\n";
+
+        /// <summary>
+        /// The raw source associated with the data provider.
+        /// </summary>
+        public string RawSource { get; private set; }
+
+        /// <summary>
+        /// Validates that a raw value exists.
+        /// </summary>
+        /// <param name="id"></param>
+        void ValidateSourceValue(uint id, bool isPretty)
+        {
+            if (!SourceValues.ContainsKey(id))
+            {
+                SourceValues[id] = Serializer.ToJsonRepresentation(JsonValues[id], isPretty);
+            }
+        }
+
+        public override void UpdateSource(bool isPretty = 
+#if DEBUG
+            true
+#else 
+            false
+#endif
+            )
+        {
+            CodeTimer timer = new CodeTimer();
+            // writing to source.
+            StringBuilder builder = new StringBuilder();
+            builder.Append(CurId.ToString());
+            timer.Mark("curid");
+            AddObjectDilimeter(isPretty, builder);
+            builder.Append(string.Join(",", Anchors.Select(a => a.ToString())));
+            timer.Mark("anchors");
+            AddObjectDilimeter(isPretty, builder);
+            bool isFirst = true;
+            ChildReferences.ForEach(kvp =>
+            {
+                if (!isFirst)
+                {
+                    builder.Append(",");
+                }
+                isFirst = false;
+                builder.Append(kvp.Key);
+                builder.Append(":");
+                builder.Append(string.Join("|", kvp.Value.Select(v => v.ToString())));
+            });
+            timer.Mark("refrences");
+            AddObjectDilimeter(isPretty, builder);
+            builder.Append(Serializer.Serialize(Binder, true, isPretty).EscapeForDelimiter(ObjectDelimiter));
+            timer.Mark("binder");
+
+            uint[] allIds = JsonValues.Keys.Union(SourceValues.Keys).OrderBy(id => id).ToArray();
+
+            allIds.ForEach(id => ValidateSourceValue(id, isPretty));
+            timer.Mark("Id validation");
+
+            foreach (uint id in allIds)
+            {
+                AddObjectDilimeter(isPretty, builder);
+                builder.Append(id.ToString());
+                AddObjectDilimeter(isPretty, builder);
+                builder.Append(SourceValues[id].EscapeForDelimiter(ObjectDelimiter));
+            }
+            timer.Mark("objects");
+            RawSource = builder.ToString();
+            timer.Mark("tosource");
+        }
+
+        private static void AddObjectDilimeter(bool isPretty, StringBuilder builder)
+        {
+            if (isPretty)
+                builder.Append(PrettySpacer);
+            builder.Append(ObjectDelimiter);
+            if (isPretty)
+                builder.Append(PrettySpacer);
+        }
+
+        /// <summary>
+        /// Reads the source for the data provider.
+        /// </summary>
+        /// <param name="source">If null, then assume empty.</param>
+        public void ReadSource(string source = null)
+        {
+            CodeTimer timer = new CodeTimer();
+            RawSource = source;
+            JsonValues = new Dictionary<uint, Documents.IJsonValue<string>>();
+            SourceValues = new Dictionary<uint, string>();
+
+            if (source.IsNullOrEmpty(true))
+            {
+                Anchors = new HashSet<uint>();
+                ChildReferences = new Dictionary<uint, HashSet<uint>>();
+                Binder = new SerializationTypeBinder<string>();
+                return;
+            }
+
+            timer.Mark("Processing defaults");
+            // splitting the source value and rewriting the source.
+            string[] objs = source.SplitByDelimiter(ObjectDelimiter).Select(v => v.UnescapeForDelimiter(ObjectDelimiter).Trim(new char[] { ' ', '\n' })).ToArray();
+            timer.Mark("Reading sources");
+            if (objs.Length < 4)
+                throw new Exception("Incorrect format when reading source.");
+
+            // reading definitions.
+            CurId = uint.Parse(objs[0]);
+            Anchors = new HashSet<uint>(objs[1].Split(',').Select(id => uint.Parse(id)));
+            timer.Mark("Anchors");
+            //ChildReferences = Serializer.Deserialize<string, Dictionary<uint, HashSet<uint>>>(objs[2]);
+            ChildReferences = new Dictionary<uint, HashSet<uint>>();
+            foreach (string crcol in objs[2].Split(','))
+            {
+                string[] kvp = crcol.Split(':');
+                if (kvp.Length != 2)
+                    continue;
+                ChildReferences[uint.Parse(kvp[0])] = new HashSet<uint>(kvp[1].Split('|').Select(v => uint.Parse(v)));
+            }
+            timer.Mark("Child refrences");
+            Binder = Serializer.Deserialize<string, SerializationTypeBinder<string>>(objs[3], true);
+            timer.Mark("Binder");
+
+            // reading the object values.
+            for (int i = 4; i < objs.Length; i += 2)
+            {
+                uint id = uint.Parse(objs[i]);
+                string val = objs[i + 1];
+                SourceValues[id] = val;
+            }
+            timer.Mark("objects");
+        }
+
+        public override string GetSource()
+        {
+            return RawSource;
+        }
+
+        /// <summary>
+        /// Clones the data provider to create a new clean data provider with the same source.
+        /// </summary>
+        /// <returns></returns>
+        public JsonStringArrayDataProvider Clone()
+        {
+            UpdateSource();
+            JsonStringArrayDataProvider provider =
+                new JsonStringArrayDataProvider(this.RawSource, Serializer);
+            return provider;
+        }
+    }
+}
---
+++ OOSerializer/Parsers/RefrenceIdParser.cs
@@ -0,0 +1,32 @@
+﻿using JCOM.OOSerializer.Documents;
+using JCOM.OOSerializer.Reference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCOM.OOSerializer.Parsers
+{
+    /// <summary>
+    /// Implementds a refrence id parser.
+    /// </summary>
+    public class RefrenceIdParser<T> : JsonObjectParser<T,RefrenceId>
+    {
+        public RefrenceIdParser()
+            : base("_rid")
+        {
+        }
+
+
+        public override Documents.JsonObjectPhrase<T> ParseFromObject(RefrenceId o, SerializationContext<T> context)
+        {
+            return new Documents.JsonObjectPhrase<T>(Identity, o.Value);
+        }
+
+        public override RefrenceId ObjectFromParse(Documents.JsonObjectPhrase<T> parse, SerializationContext<T> context)
+        {
+            return new RefrenceId((parse.Words[0] as JsonNumber<T>).As<UInt32>());
+        }
+    }
+}
